6b21aac3aa76707f492ad15d46726ab2e40f0573
mehmetaliyuksel <mehmetali864@marun.edu.tr>
2018-09-21 21:08:38 +0300

    testCommit

--- app/src/main/java/com/example/melkor/app/SplashActivity.java
+++ app/src/main/java/com/example/melkor/app/SplashActivity.java
@@ -12,6 +12,6 @@ public class SplashActivity extends AppCompatActivity {
 
         Intent intent = new Intent(SplashActivity.this,MainActivity.class);
         startActivity(intent);
-        finish();
+        finish(); //aaaaa
     }
 }
